7e17863844ff25b3b7646e00166d467a03bf47dd
Luiz Sena <luiz.sena@irede.org.br>
2024-09-26 11:15:29 -0300

    layout change

--- name-server/main.py
+++ name-server/main.py
@@ -3,29 +3,30 @@ import threading
 import Pyro4
 import Pyro4.naming
 
-debugLog = ft.Column()
-textfield = ft.TextField(label="Seu IPV4")
-button = ft.ElevatedButton("Iniciar servidor de nomes")
-debugLog.controls.append(ft.Text("Log de debug", size=25))
+logContainer = ft.Column()
+ipInputField = ft.TextField(label="Seu IP")
+connectButton = ft.ElevatedButton("Conectar")
+debugLogText = ft.Text("Debug log:", size=25, color="white")
+logContainer.controls.append(debugLogText)
 
 class NameServer:
     def __init__(self):
         self.ip     = None
 
     def set_ip(self):
-        self.ip = textfield.value
+        self.ip = ipInputField.value
         if self.ip:
             self.launch_name_server()
         else:
-            debugLog.controls.append(ft.Text("ERRO: ENTRADA VAZIA"))
-            debugLog.update()
+            logContainer.controls.append(ft.Text("ERRO: ENTRADA VAZIA", color="red"))
+            logContainer.update()
 
     def launch_name_server(self):
         threadNameServer = threading.Thread(
             target=Pyro4.naming.startNSloop, kwargs={"host": self.ip}, daemon=True
         )
-        debugLog.controls.append(ft.Text("Servidor de nomes ativo"))
-        debugLog.update()
+        logContainer.controls.append(ft.Text("Servidor de nomes ativo", color="white"))
+        logContainer.update()
         threadNameServer.start()
 
 nameServer = NameServer()
@@ -33,19 +34,22 @@ nameServer = NameServer()
 def main(page: ft.Page):
     
     page.window.width = 600
+    page.window.height = 400
+    page.theme = ft.Theme(color_scheme=ft.ColorScheme("purple", secondary="white", tertiary= "white"))
+    page.bgcolor = "white"
     
     
     def button_click(e):
         nameServer.set_ip()
         
-    button.on_click = button_click
+    connectButton.on_click = button_click
 
     page.add(ft.Column([
        ft.Row([
-           textfield,
-           button
+           ipInputField,
+           connectButton
            ]),
-        debugLog
+        ft.Container(logContainer, bgcolor=ft.colors.PURPLE_100, width=800, border_radius=5, padding=10)
     ]))
 
 ft.app(main)
